2900a1fbc988698974ec18839340a71488470d31
Marcos-WSL <marcosviniciusmoraescosta11@gmail.com>
2026-04-01 22:20:26 -0500

    Refactor Setup-Linux.sh to ensure proper directory handling and set executable permissions for premake5

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-pushd ..
+set -e
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$ROOT_DIR"
 Vendor/Binaries/Premake/Linux/premake5 --cc=clang --file=Build.lua gmake2
-popd
